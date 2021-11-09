6c745aa4b2950ecca68dd8cb9087b5d7bbbd2cc7
zcr003 <zramsey1@cnm.edu>
2021-11-09 15:02:02 -0700

    Implemented token validation & user lookup & creation

--- src/main/java/edu/cnm/deepdive/codebreaker/configuration/SecurityConfiguration.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/configuration/SecurityConfiguration.java
@@ -3,12 +3,14 @@ package edu.cnm.deepdive.codebreaker.configuration;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -25,7 +27,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   private final Converter<Jwt, ? extends AbstractAuthenticationToken> converter;
 
-  private String issuerURI;
+  @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+  private String issuerUri;
+
+  @Value("${spring.security.oauth2.resourceserver.jwt.client-id}")
   private String clientId;
 
   @Autowired
@@ -37,8 +42,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    http
         .authorizeRequests((auth) -> auth.anyRequest()
-            .anonymous())
+                .anonymous())
         .oauth2ResourceServer()
         .jwt()
         .jwtAuthenticationConverter(converter);
@@ -46,11 +54,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   //This is our decoder/ the method that will validate a token.
   public JwtDecoder jwtDecoder() {
-    NimbusJwtDecoder decoder = JwtDecoders.fromIssuerLocation(issuerURI);
+    NimbusJwtDecoder decoder = JwtDecoders.fromIssuerLocation(issuerUri);
     OAuth2TokenValidator<Jwt> audienceValidator =
         new JwtClaimValidator<List<String>>(JwtClaimNames.AUD, (aud) -> aud.contains(clientId));
     OAuth2TokenValidator<Jwt> issuerValidator =
-        JwtValidators.createDefaultWithIssuer(issuerURI);
+        JwtValidators.createDefaultWithIssuer(issuerUri);
     OAuth2TokenValidator<Jwt> combinedValidator =
         new DelegatingOAuth2TokenValidator<Jwt>(audienceValidator, issuerValidator);
     decoder.setJwtValidator(combinedValidator);
